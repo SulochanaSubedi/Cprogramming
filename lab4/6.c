e6b416b914cb49062ecd9e7f182d78e42bd81b51
SulochanaSubedi <pas081bct042@wrc.edu.np>
2024-12-27 12:11:47 +0545

    Update 6.c

--- lab4/6.c
+++ lab4/6.c
@@ -1,4 +1,4 @@
-//. Write a program to check whether the entered year is leap year or not (a year is leap if it is divisible by 4 and divisible by 100 or 400.) 
+// Write a program to check whether the entered year is leap year or not (a year is leap if it is divisible by 4 and divisible by 100 or 400.) 
 
 #include<stdio.h>
 
